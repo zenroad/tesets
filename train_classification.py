3d00529b5de4c133f861fec90c20b41934deb3aa
zhongchen <1518595517@qq.com>
2019-01-08 11:45:00 +0800

    11

--- train_classification.py
+++ train_classification.py
@@ -24,7 +24,7 @@ test_current_data, test_current_label = load_dataset.loadDataFile(TRAIN_FILES[te
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-MAX_EPOCHS = 5
+MAX_EPOCHS = 15
 BATCH_SIZE = 4
 NUM_POINT = 2048
 
@@ -69,16 +69,49 @@ for epoch in range(MAX_EPOCHS):
             out_labels = model(jittered_data)
             labels = labels.cuda()
             loss = loss_fn(out_labels, labels)
-            print(loss)
+            #print(loss)
 
             loss.backward()
 
             optimizer.step()
 
-            print("Done")
+            #print("Done")
 
     if (epoch%3 == 0):
-        pass
+        for fn in range(len(TEST_FILES)):
+            test_current_data, test_current_label = load_dataset.loadDataFile(TEST_FILES[test_file_idxs[fn]])
+            test_current_data = test_current_data[:,0:NUM_POINT,:]
+            test_current_data, test_current_label, _ = load_dataset.shuffle_data(test_current_data, np.squeeze(test_current_label))            
+            test_current_label = np.squeeze(test_current_label)
+            
 
+            file_size = test_current_data.shape[0]
+            num_batches = file_size // BATCH_SIZE
+            
+            total_correct = 0
+            total_seen = 0
+            test_loss_sum = 0
 
-
+            for batch in range(num_batches):
+                start_idx = batch * BATCH_SIZE
+                end_idx = (batch+1) * BATCH_SIZE
+                
+                rotated_data = load_dataset.rotate_point_cloud(test_current_data[start_idx:end_idx, :, :])
+                jittered_data = load_dataset.jitter_point_cloud(rotated_data)
+                jittered_data = load_dataset.random_scale_point_cloud(jittered_data)
+                jittered_data = load_dataset.rotate_perturbation_point_cloud(jittered_data)
+                jittered_data = load_dataset.shift_point_cloud(jittered_data)
+                
+                jittered_data = torch.from_numpy(jittered_data)
+                labels = torch.from_numpy(test_current_label[start_idx:end_idx]).long()
+                
+                optimizer.zero_grad()
+                
+                jittered_data = jittered_data.cuda()
+                out_labels = model(jittered_data)
+                labels = labels.cuda()
+                loss = loss_fn(out_labels, labels)
+                acc = torch.sum(out_labels==labels)/float(BATCH_SIZE*NUM_POINT)
+                print("test acc")
+                print(acc)
+                
