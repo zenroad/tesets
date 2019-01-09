14b8fe440043e3c1ab2cb8ac4344260e9211898d
zhongchen <1518595517@qq.com>
2019-01-09 09:55:37 +0800

    11

--- train_classification.py
+++ train_classification.py
@@ -35,47 +35,47 @@ optimizer = optim.Adam(model.parameters(), lr=1e-4)
 model.to(device)
 for epoch in range(MAX_EPOCHS):
 
-
-    for fn in range(len(TRAIN_FILES)):
-        current_data, current_label = load_dataset.loadDataFile(TRAIN_FILES[train_file_idxs[fn]])
-        current_data = current_data[:,0:NUM_POINT,:]
-        current_data, current_label, _ = load_dataset.shuffle_data(current_data, np.squeeze(current_label))            
-        current_label = np.squeeze(current_label)
-        
-
-        file_size = current_data.shape[0]
-        num_batches = file_size // BATCH_SIZE
-        
-        total_correct = 0
-        total_seen = 0
-        loss_sum = 0
-
-        for batch in range(num_batches):
-            start_idx = batch * BATCH_SIZE
-            end_idx = (batch+1) * BATCH_SIZE
+    if (epoch%2 != 0):
+        for fn in range(len(TRAIN_FILES)):
+            current_data, current_label = load_dataset.loadDataFile(TRAIN_FILES[train_file_idxs[fn]])
+            current_data = current_data[:,0:NUM_POINT,:]
+            current_data, current_label, _ = load_dataset.shuffle_data(current_data, np.squeeze(current_label))            
+            current_label = np.squeeze(current_label)
             
-            rotated_data = load_dataset.rotate_point_cloud(current_data[start_idx:end_idx, :, :])
-            jittered_data = load_dataset.jitter_point_cloud(rotated_data)
-            jittered_data = load_dataset.random_scale_point_cloud(jittered_data)
-            jittered_data = load_dataset.rotate_perturbation_point_cloud(jittered_data)
-            jittered_data = load_dataset.shift_point_cloud(jittered_data)
-            
-            jittered_data = torch.from_numpy(jittered_data)
-            labels = torch.from_numpy(current_label[start_idx:end_idx]).long()
-            
-            optimizer.zero_grad()
-            
-            jittered_data = jittered_data.cuda()
-            out_labels = model(jittered_data)
-            labels = labels.cuda()
-            loss = loss_fn(out_labels, labels)
-            #print(loss)
 
-            loss.backward()
+            file_size = current_data.shape[0]
+            num_batches = file_size // BATCH_SIZE
+            
+            total_correct = 0
+            total_seen = 0
+            loss_sum = 0
 
-            optimizer.step()
+            for batch in range(num_batches):
+                start_idx = batch * BATCH_SIZE
+                end_idx = (batch+1) * BATCH_SIZE
+                
+                rotated_data = load_dataset.rotate_point_cloud(current_data[start_idx:end_idx, :, :])
+                jittered_data = load_dataset.jitter_point_cloud(rotated_data)
+                jittered_data = load_dataset.random_scale_point_cloud(jittered_data)
+                jittered_data = load_dataset.rotate_perturbation_point_cloud(jittered_data)
+                jittered_data = load_dataset.shift_point_cloud(jittered_data)
+                
+                jittered_data = torch.from_numpy(jittered_data)
+                labels = torch.from_numpy(current_label[start_idx:end_idx]).long()
+                
+                optimizer.zero_grad()
+                
+                jittered_data = jittered_data.cuda()
+                out_labels = model(jittered_data)
+                labels = labels.cuda()
+                loss = loss_fn(out_labels, labels)
+                #print(loss)
 
-            #print(epoch)
+                loss.backward()
+
+                optimizer.step()
+
+                #print(epoch)
 
     if (epoch%2 == 0):
         for fn in range(len(TEST_FILES)):
