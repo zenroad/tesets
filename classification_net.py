919abcb4cec22146b6e809dfc2a47b0b7aba534c
bugsba <1518595517.com>
2019-01-12 16:12:55 +0800

    11

--- classification_net.py
+++ classification_net.py
@@ -14,6 +14,74 @@ import math
 import time
 
 
+class STN3d(nn.Module):
+    def __init__(self, num_points = 10000):
+        super(STN3d, self).__init__()
+        self.num_points = num_points
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.mp1 = torch.nn.MaxPool1d(num_points)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 9)
+        self.relu = nn.ReLU()
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.mp1(x)
+        x = x.view(-1, 1024)
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
+        if x.is_cuda:
+            iden = iden.cuda()
+        x = x + iden
+        x = x.view(-1, 3, 3)
+        return x
+
+
+class PointNetfeat(nn.Module):
+    def __init__(self, num_points = 10000, global_feat = True):
+        super(PointNetfeat, self).__init__()
+        self.stn = STN3d(num_points = num_points)
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.mp1 = torch.nn.MaxPool1d(num_points)
+        self.num_points = num_points
+        self.global_feat = global_feat
+    def forward(self, x):
+        batchsize = x.size()[0]
+        trans = self.stn(x)
+        x = x.transpose(2,1)
+        x = torch.bmm(x, trans)
+        x = x.transpose(2,1)
+        x = F.relu(self.bn1(self.conv1(x)))
+        pointfeat = x
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
+        x = self.mp1(x)
+        x = x.view(-1, 1024)
+        #if self.global_feat:
+        return x
+'''
 class PointNetfeat(nn.Module):
     def __init__(self, num_points = 2500):
         super(PointNetfeat, self).__init__()
@@ -40,7 +108,7 @@ class PointNetfeat(nn.Module):
         x = x.view(-1, 1024)
         #if self.global_feat:
         return x
-
+'''
 
 class classification_net(nn.Module):
      """docstring for edge_conv_model"""
